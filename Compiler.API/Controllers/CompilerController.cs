81ae670b8d1f5f6cc4211920a2f9b303b25af749
ilya Kupriyanov <ilya-kyp@yandex.ru>
2022-08-22 22:40:54 +0300

    попытка скомпилировать основной класс и тесты, которые для него предназначены

--- Compiler.API/Controllers/CompilerController.cs
+++ Compiler.API/Controllers/CompilerController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Emit;
+using NUnit.Framework;
 
 namespace Compiler.API.Controllers;
 
@@ -10,21 +11,40 @@ namespace Compiler.API.Controllers;
 [ApiController]
 public class CompilerController : ControllerBase
 {
-    const string classText = @"
+    const string MainClassText = @"
         using System;
 
-        public class Writer
+        public class NumberGenerator
         {
-            public void Write(string message
+            public int GenerateNum(
             {
-                Console.WriteLine(message);
+                return 1
             }
         }";
 
-    [HttpGet]
+    const string TestClassText = @"
+        using System;
+        using NUnit.Framework;
+
+        public class Tests
+        {
+            [SetUp]
+            public void Setup()
+            { }
+
+            [Test]
+            public void Test1()
+            {
+                NumberGenerator generator = new ();
+                Assert.IsTrue(2 == generator.GenerateNum());
+            }
+        }";
+
+
+    [HttpGet("[action]")]
     public IActionResult DiagnosticMethod()
     {
-        SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(classText);
+        SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(MainClassText);
         string assemblyName = Path.GetRandomFileName();
 
         MetadataReference[] references = AppDomain.CurrentDomain.GetAssemblies()
@@ -59,5 +79,46 @@ public class CompilerController : ControllerBase
         return Ok(result);
     }
 
-    public IActionResult
+    [HttpGet("[action]")]
+    public IActionResult RunTests()
+    {
+        SyntaxTree mainSyntaxTree = CSharpSyntaxTree.ParseText(MainClassText);
+        SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(TestClassText);
+
+        string assemblyName = Path.GetRandomFileName();
+
+        MetadataReference[] references = AppDomain.CurrentDomain.GetAssemblies()
+                .Where(asm => !asm.IsDynamic && !string.IsNullOrWhiteSpace(asm.Location))
+                .Select(asm => MetadataReference.CreateFromFile(asm.Location))
+                .Concat(new[]
+                {
+                    // add your app/lib specifics, e.g.:                      
+                    MetadataReference.CreateFromFile(typeof(NUnitAttribute).Assembly.Location),
+                })
+                .ToArray();
+
+        CSharpCompilation compilation = CSharpCompilation.Create(
+            assemblyName,
+            syntaxTrees: new[] { syntaxTree, mainSyntaxTree },
+            references: references,
+            options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+        List<string> result = new();
+
+        using (var memoryStream = new MemoryStream())
+        {
+            EmitResult emitResult = compilation.Emit(memoryStream);
+
+            if (!emitResult.Success)
+            {
+                IEnumerable<Diagnostic> failures = emitResult.Diagnostics.Where(diagnostic =>
+                diagnostic.IsWarningAsError ||
+                diagnostic.Severity == DiagnosticSeverity.Error);
+
+                result.AddRange(failures.Select(x => x.ToString()));
+            }
+        }
+
+        return Ok(result);
+    }
 }
